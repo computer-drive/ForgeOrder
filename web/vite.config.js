7f494bc4c1a6fae0dbe2119322c6b10c6f77c861
Haoyu <ccc123zhy@outlook.com>
2026-07-02 23:19:05 +0800

    feat: 添加mdui和mdui-icon的依赖项，处理mdui再vue中使用的问题

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default {
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // 所有以 mdui- 开头的标签名都是 mdui 组件
+          isCustomElement: (tag) => tag.startsWith('mdui-'),
+        },
+      },
+    }),
+  ],
+};
