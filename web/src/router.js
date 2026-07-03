8c6bf41e1483e649b89b7de3829581d981f1e0c4
Haoyu <ccc123zhy@outlook.com>
2026-07-03 09:34:38 +0800

    feat: 更新路由配置，添加Orders和Account组件，修复router导入方式

--- web/src/router.js
+++ web/src/router.js
@@ -1,22 +1,37 @@
-import { createBrowserRouter, createWebHashHistory } from 'vue-router'
+import { createRouter ,createWebHashHistory } from 'vue-router'
 import Home from './Home.vue'
 import Login from './Login.vue'
+import Orders from './Orders.vue'
+import Account from './Account.vue'
 
 const routes = [
     {
         path: '/',
+        name: 'Home',
         component: Home
     },
     {
         path: '/login',
+        name: 'Login',
         component: Login
+    },
+    {
+        path: '/orders',
+        name: 'Orders',
+        component: Orders
+    },
+    {
+        path: '/account',
+        name: 'Account',
+        component: Account
     }
 ]
 
-const router = createBrowserRouter(routes, {
-    history: createWebHashHistory()
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
 })
 
-
+export default router
 
 
