2e3c23aee7e20806a389a024c93da039b83235e2
Haoyu <ccc123zhy@outlook.com>
2026-07-03 14:17:34 +0800

    feat: 更新Home组件，添加餐桌状态显示和刷新功能，优化样式

--- web/src/views/main/Home.vue
+++ web/src/views/main/Home.vue
@@ -11,12 +11,12 @@
         </TipCard>
       </div>
 
-      <mdui-card variant="outlined" style="width: 100% ;margin-top:4px">
+      <mdui-card variant="outlined" style="width: 100% ;margin-top:4px; border: 0">
         <div style="margin: 8px">
 
           <div style="display: flex">
             <div style="font-size: 24px; margin-left: 12px;">今日订单</div>
-            <mdui-button-icon style="margin-left: auto;" @click="refreshOrders">
+            <mdui-button-icon style="margin-left: auto;" @click="refreshOrders" ref="refreshOrdersButton">
               <mdui-icon-refresh></mdui-icon-refresh>
             </mdui-button-icon>
           </div>
@@ -31,10 +31,49 @@
                 <div class="stats_label">总金额（元）</div>
                 <div class="stats_value">0</div>
             </div>
+            
+            <div class="stats_item">
+                <div class="stats_label">接待人数</div>
+                <div class="stats_value">0</div>
+            </div>
+
           </div>
           
         </div>
       </mdui-card>
+
+      <mdui-divider style="margin: 9px;"></mdui-divider>
+
+      <div class="table_conatiner">
+        <div style="margin: 8px">
+
+          <div style="display: flex">
+            <div style="font-size: 24px; margin-left: 12px;">餐桌状态</div>
+            <mdui-button-icon style="margin-left: auto;" @click="refreshTables" ref="refreshTablesButton">
+              <mdui-icon-refresh></mdui-icon-refresh>
+            </mdui-button-icon>
+          </div>
+          
+          <div class="table_scroll">
+            <div class="table_content">
+              <div v-for="i in 12" :key="i" class="table_item">
+                <div class="table_label">餐桌 {{ i }}</div>
+                <div class="table_value">空闲</div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+
+
+
+      <mdui-button full-width style="height: 50px;">
+          <div style="font-size: 16px">出餐</div>
+          <mdui-icon-done slot="icon" ></mdui-icon-done>
+      </mdui-button>
+
+
     </div>
 
 </template> 
@@ -42,11 +81,17 @@
 <script setup>
 import TipCard from '@/components/TipCard.vue'
 
+import 'mdui/components/card.js'
+import 'mdui/components/divider.js';
+import 'mdui/components/button.js'
+import 'mdui/components/collapse.js';
+import 'mdui/components/collapse-item.js';
+import 'mdui/components/list.js'
 
 
-import 'mdui/components/card.js';
-import '@mdui/icons/warning.js';
-import '@mdui/icons/refresh.js';
+import '@mdui/icons/warning.js'
+import '@mdui/icons/refresh.js'
+import '@mdui/icons/done.js';
 
 import { ref, onMounted } from 'vue'
 
@@ -54,10 +99,27 @@ const currentUser = ref('用户'); //
 
 const isBusiness = ref(false); // 是否营业
 
-const refreshOrders = () => {
-  console.log('刷新订单');
-  // 在这里添加刷新订单的逻辑
+const refreshOrdersButton = ref(null);
+const refreshTablesButton = ref(null);
+
+
+
+const refreshOrders = async () => {
+  refreshOrdersButton.value.loading = true;
+
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  refreshOrdersButton.value.loading = false;
+
 };
+
+const refreshTables = async () => {
+  refreshTablesButton.value.loading = true;
+
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  refreshTablesButton.value.loading = false;
+};
+
+
 
 </script>
 
@@ -75,7 +137,7 @@ const refreshOrders = () => {
 
 
   .stats_label {
-    font-size: 16px;
+    font-size: 14px;
     text-align: center;
 
   }
@@ -84,4 +146,43 @@ const refreshOrders = () => {
     font-size: 24px;
     text-align: center;
   }
+
+  .table_conatiner {
+    width: 100% ;
+    margin-top:4px;
+    border: 0;
+    
+  }
+
+
+  .table_scroll {
+    overflow-x: auto;
+    overflow-y: hidden;
+    width: 100%
+  }
+
+  .table_content {
+    display: flex;
+    justify-content: center;
+    min-width: max-content;
+    gap: 8px;
+
+  
+  
+  }
+
+  .table_item {
+    /* width: 25%; */
+  }
+
+  .table_label {
+    font-size: 14px;
+    text-align: center;
+  }
+
+  .table_value {
+    font-size: 18px;
+    text-align: center;
+  }
+
 </style>
