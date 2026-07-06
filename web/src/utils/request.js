0e303ed800702bfd932fe8abb643abe5ef2750c0
Haoyu <ccc123zhy@outlook.com>
2026-07-06 16:43:26 +0800

    feat: 移除测试代码

--- web/src/utils/request.js
+++ web/src/utils/request.js
@@ -10,7 +10,7 @@ const request = axios.create({
 request.interceptors.request.use(
     config => {
         const token = localStorage.getItem('token')
-        console.log(token)
+        // console.log(token)
         if (token) {
             config.headers.Authorization = `Bearer ${token}`
         }
