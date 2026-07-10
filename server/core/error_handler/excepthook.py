349c48f7a1a2d01b221ff0939f1eb16d11b2cf95
Haoyu <ccc123zhy@outlook.com>
2026-07-10 21:20:54 +0800

    fix: 修复子线程的excepthook设置问题 refactor：为excepthook添加子线程文本

--- server/core/error_handler/excepthook.py
+++ server/core/error_handler/excepthook.py
@@ -8,11 +8,24 @@ from core.log.logger import Logger
 from .error_report import generate_error_report
 
 
-def excepthook(type, value, tb, is_threading: bool = False):
+def excepthook(type, value, tb, thread: threading.Thread = None):
     import extensions
 
     if hasattr(extensions, 'logger') and isinstance(extensions.logger, Logger):
-        extensions.logger.error(
+        
+        if thread:
+            extensions.logger.error(
+                {
+                "type": type.__name__,
+                "value": traceback.format_exception(type, value, tb),
+                "thread": thread.name,
+            }, 
+            class_name="ERROR_HANDLER",
+            method="ThreadedUncaughtException",
+        )
+            
+        else:
+            extensions.logger.error(
             {
                 "type": type.__name__,
                 "value": traceback.format_exception(type, value, tb),
@@ -22,18 +35,21 @@ def excepthook(type, value, tb, is_threading: bool = False):
             method="UncaughtException",
         )
     else:
-        print(f"Uncaught exception: {type.__name__}")
+        print(f"{'Threaded' if thread else ''} Uncaught exception: {type.__name__} in thread '{thread.name if thread else ''}'")
         traceback.print_tb(tb)
 
     generate_error_report(
         error_type="critical",
-        error_title=f"{'Threaded ' if is_threading else ''}{'Uncaught Exception' if is_threading else 'Uncaught Exception'}",
+        error_title=f"{'Threaded ' if thread else ''}{'Uncaught Exception' if thread else 'Uncaught Exception'}",
         errpr_description=str(value),
         error_detail=traceback.format_exception(type, value, tb),
         time=datetime.datetime.now(),
     )
 
+def thread_excepthook(type, value, tb, thread: threading.Thread):
+    excepthook(type, value, tb, thread)
+
 def install():
     sys.excepthook = excepthook
 
-    threading.excepthook = lambda type, value, tb: excepthook(type, value, tb, is_threading=True)
+    threading.excepthook = thread_excepthook
