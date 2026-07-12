852b4a5a5d6624e53fed8c22f2289b2b9dd0ea9e
Haoyu <ccc123zhy@outlook.com>
2026-07-12 14:06:39 +0800

    feat: 更新DishEdit组件，添加菜品描述、分类选择和选项管理功能

--- web/src/views/shop/DishEdit.vue
+++ web/src/views/shop/DishEdit.vue
@@ -7,16 +7,17 @@
     @confirm="dishData.price = $event"
      ></NumberKeyboardDialog>
     <div class="container mdui-prose">
-        <h1>编辑“{{ dishData.name }}”</h1>
+        <h2>编辑“{{ dishData.name }}”</h2>  
         <div style="margin-bottom: 24px; font-size: 18px">在下方更改菜品信息，点击右上角的“保存”按钮以应用更改。    </div>
 
         <div>
 
-            <div class="setting-item">
+            <div class="setting-item" ref="nameInput">
                 <div class="setting-item-key">菜品名称</div>
                 <div class="setting-item-value">
                     
                     <mdui-text-field 
+                    v-model="dishData.name"
                     variant="outlined" ></mdui-text-field>
             
                 </div>
@@ -29,6 +30,7 @@
                     autosize
                     min-rows="1"
                     max-rows="3"
+                    v-model="dishData.description"
                     variant="outlined"></mdui-text-field>
             </div>
 
@@ -48,48 +50,64 @@
 
             <div class="setting-item">
                 启用此菜品
-                <mdui-switch></mdui-switch>
+                <mdui-switch :checked="dishData.is_available" ref="isAvailableSwitch"></mdui-switch>
             </div>
 
             <div class="setting-item">
                 分类
-                <mdui-select value="1" variant="outlined" class="setting-item-value">
-                    <mdui-menu-item value="1">Item 1</mdui-menu-item>
-                    <mdui-menu-item value="2">Item 2</mdui-menu-item>
+                <mdui-select 
+                    ref="categoryInput"
+                    variant="outlined" 
+                    class="setting-item-value"
+                    
+                >
+                    <mdui-menu-item 
+                        v-for="category in categories" 
+                        :key="category.id"
+                        :value="category.id"
+                    >{{ category.name }}</mdui-menu-item>
                     <mdui-icon-arrow-drop-down slot="end-icon"></mdui-icon-arrow-drop-down>
                 </mdui-select>
+                <!-- <div v-else>加载中</div> -->
             </div>
 
             <div class="setting-item">
                 选项
-                <mdui-button-icon>
+                <mdui-button-icon @click="addNewChoices">
                     <mdui-icon-add></mdui-icon-add>
                 </mdui-button-icon>
             </div>    
 
             <mdui-list>
-                    <mdui-list-item nonclickable rounded class="choices-item">
-                        <div style="display: flex; justify-content: space-between; align-items: center; gap: 10px">    
-                            <div style="flex-shrink: 0;">是否可以   </div>
-                            
-                            <div style="display: flex; align-items: center; gap: 6px">
+                    <div v-for="(options, name) in dishData.choices">
+
+                        <mdui-list-item nonclickable rounded class="choices-item">
+
+                            <div style="display: flex; justify-content: space-between; align-items: center; gap: 10px">    
+                               
+                                <div style="flex-shrink: 0;">{{ name }}</div>
                                 
-                                <div style="display:flex; flex-wrap: wrap; gap: 6px;">
-                                    <mdui-chip style="padding:0px">不辣</mdui-chip>
-                                    <mdui-chip >不辣</mdui-chip>
-                                    <mdui-chip >不辣</mdui-chip>
-                                    <mdui-chip >不辣</mdui-chip>
+                                <div style="display: flex; align-items: center; gap: 6px">
+                                    
+                                    <div style="display:flex; flex-wrap: wrap; gap: 6px;">
+                                        <mdui-chip v-for="option in options" @click="deleteOption(name, option)">{{ option }}</mdui-chip>
+                                    </div>
+ 
+
                                 </div>
 
-                                <mdui-button-icon variant="filled">
-                                    <mdui-icon-add></mdui-icon-add>
-                                </mdui-button-icon>
+                                <div style="display: flex; gap: 8px">
+                                    <mdui-button-icon variant="tonal" @click="addNewOption(name)">
+                                            <mdui-icon-add></mdui-icon-add>
+                                    </mdui-button-icon>
+
+                                    
+                                </div>
 
                             </div>
-                        </div>
-                        
 
-                    </mdui-list-item>
+                        </mdui-list-item>
+                    </div>
 
              </mdui-list>    
 
@@ -104,6 +122,7 @@
     import '@/assets/shop.dish_edit.css'
 
     import { ref, inject, h, onMounted, onBeforeUnmount } from 'vue'
+    import { useRoute, useRouter } from 'vue-router'
 
     import 'mdui/components/text-field.js'
     import 'mdui/components/dialog.js'
@@ -113,18 +132,25 @@
     import 'mdui/components/switch.js'
     import 'mdui/components/select.js'
     import 'mdui/components/menu-item.js'
-    import 'mdui/components/button.js'
+    
     import 'mdui/components/chip.js'
 
+    import { prompt } from 'mdui/functions/prompt.js'
+    import { dialog } from 'mdui/functions/dialog.js';
+    import { snackbar } from 'mdui/functions/snackbar.js';
+
     import '@mdui/icons/edit.js'
-    import '@mdui/icons/backspace.js'
-    import '@mdui/icons/clear.js'
-    import '@mdui/icons/keyboard-return.js'
+    
     import '@mdui/icons/arrow-drop-down.js'
     import '@mdui/icons/add.js'
     import '@mdui/icons/save.js'
+    import '@mdui/icons/delete.js'
 
     import TopProgressBar from '@/components/TopProgressBar.vue'
+    
+    import NumberKeyboardDialog from '@/components/NumberKeyboardDialog.vue'
+    
+    import request from '@/utils/request.js'
 
     defineProps({
         id: {
@@ -137,86 +163,275 @@
         }
     })
 
+    const route = useRoute()
+    const router = useRouter()
+
     const { setRightComponent, clearRightComponent } = inject('rightComponent')
 
     const isLoading = ref(false)
 
-    const dishData = ref({})
-    const price = ref(0)
-
     const priceInputDialog = ref(null)
-    const currentText = ref('0')
+
+    const categoryInput = ref(null)
+    const nameInput = ref(null)
+    const isAvailableSwitch = ref(null)
+    const saveButton = ref(null)
+
+    const dishData = ref({})
+    let originDishData = {}
+    const categories = ref([])
+    let choicesChanging = []
+    
 
 
-    const openPriceInputDialog = () => {
-        currentText.value = price.value.toString()
-        priceInputDialog.value.open = true
+    // 右上角的按钮
+    const handleSave = (event) => {
+        // 通过事件对象获取按钮元素
+        const button = event?.currentTarget
+    
+        if (button) {
+            button.loading = true
+        }
         
-    }
-
-    const inputText = (text) => {
-        if (currentText.value == '0') {
-            currentText.value = text
-        } else {
-            if (text == '.' ) {
-                if (currentText.value.indexOf('.') != -1) {
-                    // 已存在小数点，不允许再输入
-                    return 
-                }
-            }
-
-            if (currentText.value.indexOf('.') != -1) {
-                // 已存在小数点，判断小数点后的数字是否超过两位
-                if (currentText.value.length - currentText.value.indexOf('.') > 2) {
-                    // 小数点后的数字超过两位，不允许再输入
-                    return 
-                }
-            }
-
-            
-            currentText.value += text
-        }
-    }
-
-    const clearText = () => {
-        currentText.value = '0'
-    }
-
-    const backspaceText = () => {
-        if (currentText.value.length > 1) {
-            currentText.value = currentText.value.slice(0, -1)
-        } else {
-            currentText.value = '0'
-        }
-    }
-
-    const handleSubmit = () => {
-        priceInputDialog.value.open = false
-        price.value = Number(currentText.value)
-        // currentText.value = '0'
-    }
-
-    const handleSave = () => {
         // 保存菜品数据
+        let dishData_ = {...dishData.value,}
+
+        dishData_.price *= 100 // 转换为分
+        dishData_.is_available = isAvailableSwitch.value.checked
+
+        if (typeof categoryInput.value.value == 'string') {
+            let category = -1 
+            for ( let i = 0; i < categories.value.length; i++) {
+                if (categories.value[i].name == categoryInput.value.value) {
+                    category = i
+                    break
+                }
+            }
+            if (category == -1) {
+                snackbar({
+                    message: '分类不存在'
+                })
+                return
+            }
+            dishData_.category = category
+        } else {
+            dishData_.category = Number(categoryInput.value.value)
+        }
+
+        // 获取被修改的项
+        let changed = {}
+
+        // 名称
+        if (dishData_.name == '') {
+            nameInput.setCustomValidity("名称不能为空")
+        } else if (dishData_.name !== originDishData.name) {
+            changed.name = dishData_.name
+        }
+
+        // 描述
+        if (dishData_.description !== originDishData.description) {
+            changed.description = dishData_.description
+        }
+
+        // 价格
+        if (dishData_.price <= 0) {
+            priceInputDialog.value.setCustomValidity("价格必须大于0")
+        } else if (dishData_.price !== originDishData.price) {
+            changed.price = dishData_.price
+        }
+
+        // 是否启用
+        if (dishData_.is_available !== originDishData.is_available) {
+            changed.is_available = dishData_.is_available
+        }
+
+        // 分类
+        if (dishData_.category !== originDishData.category) {
+            changed.category = dishData_.category
+        }
+
+
+        
+    
+        button.loading = false
+
+
     }
 
 
-    onMounted(() => {
+    onMounted( async () => {
         // 设置居右的保存按钮组件
         setRightComponent(h('mdui-button-icon', {
-            onClick: handleSave
+            onClick: handleSave,
+            ref: (el) => {saveButton.value = el}
         }, [
             h('mdui-icon-save')
         ]))
 
-        // 获取菜品数据
+        // 获取菜品数据和分类信息
         isLoading.value = true
 
+        const dishId = route.params.id
 
-    })
+        try {
+            // 获取菜品数据
+            let res = await request.post(`/shop/dishes/get`, {
+                id: Number(dishId),
+            })
+            if (res.data.status == 0) {
+                dishData.value = res.data.data
+
+                originDishData = {...res.data.data}
+
+                dishData.value.price /= 100 // 转换为元
+                
+
+                
+
+                // console.log(dishData.value)
+            }
+
+            // 获取分类信息
+            res = await request.get(`/shop/dishes/getAllCategories`)
+            if (res.data.status == 0) {
+                categories.value = res.data.data
+                // console.log(categories.value)
+                // console.log(categories.value[dishData.value.category].name)
+                categoryInput.value.value = categories.value[dishData.value.category].name
+            }
+
+
+        } catch (error) {
+            console.log(error)
+        } finally {
+            isLoading.value = false
+        }
+
+        
+
+    })    
 
     onBeforeUnmount(() => {
         clearRightComponent()
     })
+
+    // 添加选项
+    const addNewChoices = () => {
+        prompt({
+            headline: '添加选项',
+            description: '请输入新选项的名称',
+            confirmText: '确定',
+            cancelText: '取消',
+            onConfirm: (name) => {
+                if (name == '') {
+                    snackbar({
+                        message: '选项名称不能为空'
+                    })
+                } else if (dishData.value.choices[name]) {
+                    snackbar({
+                        message: '选项已存在'
+                    })
+                } else {
+                    dishData.value.choices[name] = []
+
+                    choicesChanging.push({
+                        type: "new_choice",
+                        name: name,
+                    })
+                }
+            }
+        })
+    }
+
+    // 添加选项的选项
+    const addNewOption = (name) => {
+        prompt({
+            headline: `添加“${name}”的项目`,
+            description: '请输入新项目的名称',
+            confirmText: '确定',
+            cancelText: '取消',
+            onConfirm: (option) => {
+                if (option == '') {
+                    snackbar({
+                        message: '项目名称不能为空'
+                    })
+                } else if (dishData.value.choices[name].includes(option)) {
+                    snackbar({
+                        message: '项目已存在'
+                    })
+                } else {
+                    dishData.value.choices[name].push(option)
+                    
+                    choicesChanging.push({
+                        type: "new_option",
+                        name: name,
+                        option: option,
+                    })
+                }
+            }
+        })
+    }
+
+    // 删除选项的选项
+    const deleteOption = (name, option) => {
+        let headline = ''
+        let description = ''
+
+        // 判断长度是否为一
+        if (dishData.value.choices[name].length == 1) {
+            // headline和description显示为删除Choices
+            headline = `删除“${name}”`
+            description = `选项${name}将剩余一个项目，删除${option}将删除该选项，确定继续吗？`
+        } else {
+            // headline和description显示为删除项目
+            headline = `删除“${name}”的项目`
+            description = `确定删除项目“${option}”吗？`
+        }
+
+        dialog({
+            headline: headline,
+            description: description,
+            actions: [
+                {
+                    text: "取消",
+                },
+                {
+                    text: "确定",
+                    onClick: () => {
+                        const index = dishData.value.choices[name].indexOf(option)
+                        dishData.value.choices[name].splice(index, 1)
+
+                        if (dishData.value.choices[name].length == 0) {
+                            // 选项中没有项目了，删除选项
+                            delete dishData.value.choices[name]
+                            
+                            choicesChanging.push({
+                            type: "delete_choice",
+                            name: name,
+                            option: option,
+                        })
+                        }
+                         else {
+                            // 选项中还有项目，不删除选项
+                            
+                            // 判断之前有没有添加项目的操作
+                            if (choicesChanging.find(choice => choice.name == name && choice.type == "new_option")) {
+                                // 之前有添加项目的操作，删除之前的操作记录
+                                choicesChanging.splice(choicesChanging.findIndex(choice => choice.name == name && choice.type == "new_option"), 1)
+                            } else {
+                                // 之前没有添加项目的操作，添加项目的操作记录
+                                choicesChanging.push({
+                                    type: "delete_option",
+                                    name: name,
+                                    option: option,
+                                })
+                            }
+                        }
+                    },
+                },
+                
+            ]
+        })
+    }
 
    </script>
