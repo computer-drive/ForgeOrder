8364cb5bcfe9238471da39a0d71169eebb2f7ecf
Haoyu <ccc123zhy@outlook.com>
2026-07-13 22:13:40 +0800

    refactor: 重构错误报告生成逻辑，使用 ID 计数替代 UUID，简化代码结构

--- server/core/error_handler/error_report.py
+++ server/core/error_handler/error_report.py
@@ -1,11 +1,11 @@
 import datetime
-from os import error
 from typing import Literal
 import json
-from core.log.logger import Logger
-import uuid
 import sys
 import os
+
+from core.log.logger import Logger
+
 
 def generate_error_report(
     error_type: Literal["error", "critical"],
@@ -15,10 +15,22 @@ def generate_error_report(
     time: datetime.datetime,
 ):
     
-    error_id = str(uuid.uuid4())
+    
+    error_file = os.path.join(f"data/error_reports/{datetime.datetime.now().strftime("%Y-%m-%d")}.json")
 
+    os.makedirs("data/error_reports", exist_ok=True)
+
+    with open(error_file, 'r', encoding='utf-8') as f:
+
+        try:
+            data = json.load(f)
+            
+        except json.JSONDecodeError:
+            data = []
+
+    
     error_report = {
-        "id": error_id,
+        "id": len(data) + 1,
         "error_info": {
             "type": error_type,
             "title": error_title,
@@ -28,27 +40,20 @@ def generate_error_report(
         },
         "sys_info": {
             "os": sys.platform,
-            "python": sys.version,
-            
+            "python": sys.version,  
         }
     }
 
-    import extensions
-    if hasattr(extensions, 'logger')  and isinstance(extensions.logger, Logger):
-        extensions.logger.info(
-            f"Generated error report: {error_id}",
-            class_name="ERROR_HANDLER",
-            method="GeneratedErrorReport",
-        )
-    else:
-        print(f"Generated error report: {error_id}")
+    data.append(error_report)
 
-    if not os.path.exists("data/error_reports"):
-        os.makedirs("data/error_reports")
-        
+    with open(error_file, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(data, ensure_ascii=False, indent=4))
+
+
+
+
+
     
-    with open(f"data/error_reports/{error_id}.json", "w", encoding="utf-8") as f:
-        json.dump(error_report, f, indent=4, ensure_ascii=False)
 
     
 
