ad69455163078e4c52e3ac705cf2174a7198b955
Haoyu <ccc123zhy@outlook.com>
2026-07-15 09:42:50 +0800

    feat: 添加处理ExecuteError和SqliteError的错误处理函数

--- server/app/init_app/errorhandler.py
+++ server/app/init_app/errorhandler.py
@@ -7,6 +7,8 @@ from ..db.db import close_databases
 from core.utils import make_response
 import extensions
 import traceback
+import sqlite3
+from core.db.exceptions import ExecuteError
 
 def unsupported_media_type(e: UnsupportedMediaType):
         return make_response(
@@ -41,6 +43,23 @@ def argument_exception(e):
             e.args_,
         ), 400
     
+def handle_execute_error(e: ExecuteError):
+    extensions.logger.error(
+        {"sql": e.sql, "origin_error": str(e.origin_error)},
+        "FLASK_APP",
+        "ExecuteError"
+    )
+    return make_response(9002, 500), 500
+
+
+def handle_sqlite_error(e: sqlite3.Error):
+    extensions.logger.error(
+        {"type": type(e).__name__, "msg": str(e)},
+        "FLASK_APP",
+        "SqliteError"
+    )
+    return make_response(9002, 500), 500
+
 
 def teardown_appcontext(error):
         close_databases()
