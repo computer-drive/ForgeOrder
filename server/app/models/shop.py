d91881e99c3aaae9314455c7496737e48ea67e7f
Haoyu <ccc123zhy@outlook.com>
2026-07-20 10:15:41 +0800

    fix: 注释掉删除类别函数中的traceback打印，减少不必要的错误输出

--- server/app/models/shop.py
+++ server/app/models/shop.py
@@ -274,8 +274,8 @@ def delete_category():
         ), 200
     
     except NotFoundError as e :
-        import traceback
-        traceback.print_exc()
+        # import traceback
+        # traceback.print_exc()
         return make_response(
             3001,
             None
