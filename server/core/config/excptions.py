ac61094e84873d33afa84a8f392a0eb8ce720d9e
Haoyu <ccc123zhy@outlook.com>
2026-07-21 19:51:32 +0800

    feat: 添加UnsupportedTypeError异常类，改进VerifyHandler的类型验证逻辑

--- server/core/config/excptions.py
+++ server/core/config/excptions.py
@@ -1,6 +1,27 @@
+from math import exp
+
+
 class UnsupportedVerifyHandlerError(Exception):
+
+    '''
+    使用FunctionHandler，函数的返回值结构不正确
+    '''
     def __init__(self, verify_handler_class: type):
         self.verify_handler_class = verify_handler_class
         
         super().__init__(f"Implemented {verify_handler_class.__name__} in an unsupported way.")
    
+
+class UnsupportedTypeError(Exception):
+    '''
+    该处理器无法处理这个值的类型。
+    '''
+    def __init__(self, verify_handler_class: type, expected_type: type, value_type: type):
+        self.verify_handler_class = verify_handler_class
+        self.expected_type = expected_type
+        self.expected_type = expected_type
+        self.value_type = value_type
+
+        super().__init__(
+            f"Handler '{self.verify_handler_class.__name__}' cannot deal with type {value_type}, expected type is {expected_type}."
+        )
--- server/core/config/validation.py
+++ server/core/config/validation.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
-from typing import Any, Callable
-from .excptions import UnsupportedVerifyHandlerError
+from typing import Any, Callable, Union
+
+from .excptions import UnsupportedTypeError, UnsupportedVerifyHandlerError
 
 
 @dataclass
@@ -10,16 +11,27 @@ class SettingsProperty:
     default: Any
     verify: VerifyHandler
 
-class VerifyHandler: 
+class VerifyHandler:
+    allow_types : type | None = None # None 表示接收任意类型
+
     def verify(self, value: Any) -> tuple[bool, str]:
+        if self.allow_types is None or isinstance(value, self.allow_types):
+            return self._verify(value)
+        else:
+            raise UnsupportedTypeError(self, self.allow_types, type(value)) #type: ignore
+        
+    def _verify(self, value: Any) -> tuple[bool, str]: #type: ignore
         pass
+
 
 class NotEmpty(VerifyHandler):
     '''
     不可为空。
     允许的类型：str | None
     '''
-    def verify(self, value: Any) -> tuple[bool, str]:
+    allow_types = str | None #type: ignore
+
+    def _verify(self, value: Any) -> tuple[bool, str]:
         if value is not None and isinstance(value, str) and value.strip() != "":
             return True, ""
         else:
@@ -30,11 +42,13 @@ class Range(VerifyHandler):
     限制值在指定范围内。
     允许的类型：float | int
     '''
+    allow_types = float | int #type: ignore
+
     def __init__(self, min_value: float | int, max_value: float | int):
         self.min_value = min_value
         self.max_value = max_value
         
-    def verify(self, value: Any) -> tuple[bool, str]:
+    def _verify(self, value: Any) -> tuple[bool, str]:
         if not isinstance(value, (float, int)):
             return False, f"value must be float or int type"
         
@@ -48,11 +62,13 @@ class Length(VerifyHandler):
     限制值长度在指定范围内。
     允许的类型：str
     '''
+    allow_types = str
+
     def __init__(self, min_value: int, max_value: int):
         self.min_value = min_value
         self.max_value = max_value
         
-    def verify(self, value: Any) -> tuple[bool, str]:
+    def _verify(self, value: Any) -> tuple[bool, str]:
         if not isinstance(value, str):
             return False, f"value must be str type"
 
@@ -66,20 +82,28 @@ class Choices(VerifyHandler):
     限制值只能是指定的选项。
     允许的类型：Any
     '''
+    allow_types = None
+
     def __init__(self, *choices):
         self.choices = choices
         
-    def verify(self, value: Any) -> tuple[bool, str]:
+    def _verify(self, value: Any) -> tuple[bool, str]:
         if value in self.choices:
             return True, ""
         else:
             return False, f"value must be {', '.join(self.choices)}"
 
 class FunctionHandler(VerifyHandler):
+    '''
+    自定义验证器。
+    允许的类型：Any
+    '''
+    allow_types = None
+
     def __init__(self, func: Callable):
         self.func = func
         
-    def verify(self, value: Any) -> tuple[bool, str]:
+    def _verify(self, value: Any) -> tuple[bool, str]:
         result = self.func(value)
         
         if isinstance(result, tuple) and len(result) == 2:
@@ -95,10 +119,12 @@ class AnyOf(VerifyHandler):
     限制值必须匹配任意一个验证器。
     允许的类型：Any
     '''
+    allow_types = None
+    
     def __init__(self, *verify_handlers: VerifyHandler):
         self.verify_handlers = verify_handlers
     
-    def verify(self, value: Any) -> tuple[bool, str]:
+    def _verify(self, value: Any) -> tuple[bool, str]:
         errors = []
         
         for verify_handler in self.verify_handlers:
@@ -116,27 +142,17 @@ class AllOf(VerifyHandler):
 
     允许的类型：Any
     '''
+    allow_types = None
+    
     def __init__(self, *verify_handlers):
         self.verify_handlers: tuple[VerifyHandler] = verify_handlers
     
-    def verify(self, value: Any) -> tuple[bool, str]:
-        result = self.verify_handler.verify(value)
-        if result[0]:
-            return False, result[1]
+    def _verify(self, value: Any) -> tuple[bool, str]:
+        for verify_handler in self.verify_handlers:
+            result = verify_handler.verify(value)
+            if result[0]:
+                return False, result[1]
         else:
             return True, ""
         
-class Type(VerifyHandler):
-    def __init__(self, typing: type):
-        self.typing = typing
-        
-    def verify(self, value: Any) -> tuple[bool, str]:
-        if not isinstance(value, self.typing):
-            return False, f"value must be {self.typing.__name__} type"
-        else:
-            return True, ""
-        
-
-
-
-    
+   
