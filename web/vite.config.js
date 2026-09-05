dfed436f6944ea0c2fda93b224f391d6aecb5078
haoyu <ccc123zhy@outlook.com>
2026-09-05 17:45:44 +0800

    feat: 移除vite配置中的代理服务器设置

--- web/vite.config.js
+++ web/vite.config.js
@@ -17,23 +17,6 @@ export default {
           '@': fileURLToPath(new URL('./src', import.meta.url)),
         }
       },
-  server : {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        configure: (proxy) => {
-          // 获取真实ip并设置header
-          proxy.on('proxyReq', (proxyReq, req, res, options) => {
-            const ip = req.socket?.remoteAddress || req.connection?.remoteAddress || 'unknown';
-            const realIP = ip.replace(/^::ffff:/, '');
-            proxyReq.setHeader('X-Real-IP', realIP);
-            proxyReq.setHeader('X-Forwarded-For', realIP);
-          })
-        }
-      }
-    }
-  },
   build: {
     outDir: '../server/static',
   },
